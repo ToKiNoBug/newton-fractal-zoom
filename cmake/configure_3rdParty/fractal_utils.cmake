e0b470fd61097e04a7e19d534fb6728a28643f32
ToKiNoBug <tokinobug@163.com>
2023-06-27 00:58:50 +0800

    update to fractal_utils 2.3.8

--- cmake/configure_3rdParty/fractal_utils.cmake
+++ cmake/configure_3rdParty/fractal_utils.cmake
@@ -9,7 +9,7 @@ include(FetchContent)
 message(STATUS "Downloading fractal_utils...")
 FetchContent_Declare(fractal_utils
         GIT_REPOSITORY https://github.com/ToKiNoBug/FractalUtils.git
-        GIT_TAG v2.3.7
+        GIT_TAG v2.3.8
         OVERRIDE_FIND_PACKAGE)
 
 FetchContent_MakeAvailable(fractal_utils)
