5e362e043eee97251bf38b26ab758f50c53284e9
ToKiNoBug <tokinobug@163.com>
2023-06-29 22:21:05 +0800

    fix cpu renderer

--- newton_render/cpu_renderer.cpp
+++ newton_render/cpu_renderer.cpp
@@ -34,15 +34,29 @@ void cpu_renderer::render(const newton_fractal::render_config &config,
 
   normalize_option mag_opt{INFINITY, -INFINITY}, arg_opt{INFINITY, -INFINITY};
 
-  for (int r = skip_rows; r < rows - skip_rows; r++) {
-    for (int c = skip_cols; c < cols - skip_cols; c++) {
-      const auto norm_arg = this->m_map_norm_arg.at<std::complex<double>>(r, c);
-      const double mag = norm_arg.real();
-      const double arg = norm_arg.imag();
+  {
+    int64_t processed_count = 0;
+    for (int r = skip_rows; r < rows - skip_rows; r++) {
+      for (int c = skip_cols; c < cols - skip_cols; c++) {
+        if (!map_has_value.at<bool>(r, c)) {
+          continue;
+        }
+        const auto norm_arg =
+            this->m_map_norm_arg.at<std::complex<double>>(r, c);
+        const double mag = norm_arg.real();
+        const double arg = norm_arg.imag();
 
-      mag_opt.add_data(mag);
-      arg_opt.add_data(arg);
+        mag_opt.add_data(mag);
+        arg_opt.add_data(arg);
+        processed_count++;
+      }
     }
+    if (processed_count <= 0) {
+      mag_opt.min = 0;
+      arg_opt.min = 0;
+    }
+    mag_opt.max = std::max(mag_opt.max, std::nextafter(mag_opt.min, 1));
+    arg_opt.max = std::max(arg_opt.max, std::nextafter(arg_opt.min, 1));
   }
 
   for (int r = skip_rows; r < rows - skip_rows; r++) {
@@ -53,7 +67,7 @@ void cpu_renderer::render(const newton_fractal::render_config &config,
 
       image_u8c3.at<fractal_utils::pixel_RGB>(r, c) =
           render_cpu(config, map_has_value.at<bool>(r, c),
-                     map_nearest_idx.at<uint8_t>(r, c), mag_opt.normalize(mag),
+                     map_nearest_idx.at<uint8_t>(r, c), arg_opt.normalize(mag),
                      arg_opt.normalize(arg));
     }
   }
