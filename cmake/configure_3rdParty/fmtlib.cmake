e05ac458601a6205e75316e51249deb9308e26d9
虫 <tokinobug@163.com>
2023-07-04 17:48:06 +0800

    add installation

--- cmake/configure_3rdParty/fmtlib.cmake
+++ cmake/configure_3rdParty/fmtlib.cmake
@@ -1,16 +1,16 @@
 find_package(fmt QUIET)
 
-if(${fmt_FOUND})
+if (${fmt_FOUND})
     return()
-endif()
+endif ()
 
 include(FetchContent)
-
+set(FMT_MASTER_PROJECT OFF)
 message(STATUS "Downloading fmtlib...")
 FetchContent_Declare(fmt
-    GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-    GIT_TAG 10.0.0
-    OVERRIDE_FIND_PACKAGE)
+        GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+        GIT_TAG 10.0.0
+        OVERRIDE_FIND_PACKAGE)
 
 FetchContent_MakeAvailable(fmt)
 
